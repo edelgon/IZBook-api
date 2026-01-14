cae26fbfd14fca9deadac15d889443d5605b9a2d
edelgon <edelgon@gmail.com>
2026-01-14 16:08:48 +0000

    first commit

---
+++ src/main/java/com/iz/IZBook/IzBookApplication.java
@@ -0,0 +1,13 @@
+package com.iz.IZBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iz/IZBook/IzBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iz.IZBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
